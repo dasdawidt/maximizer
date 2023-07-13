354c572a86d2bdbc162a123f6c141cbc8c909ef7
dasdawidt <David_Maier@gmx.de>
2023-07-13 19:43:41 -0400

    feat: dynamically add/remove listeners i. o. flag

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// add a context menu item on chrome startup
+// Add a context menu item on chrome startup
 chrome.runtime.onInstalled.addListener(() => {
     const contexts = [
         'page',
@@ -17,9 +17,8 @@ chrome.runtime.onInstalled.addListener(() => {
     });
 });
 
-// when the maximizer context menu item is clicked
+// When the maximizer context menu item is clicked, execute the maximizer on that tab
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    //chrome.tabs.sendMessage(tab.id, "maximizerTriggered", {frameId: info.frameId});
     if (!tab.url.includes('chrome://')) {
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
@@ -28,24 +27,31 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
     }
 });
 
+// Executes the maximizer
 function executeMaximizer() {
+
     const selectionColorRGB = '250, 128, 114';
 
-    var maximizerSelectionActive = true;
+    const listeners = [
+        ['mouseover', highlightElement, false],
+        ['mouseout', unhighlightElement, false],
+        ['click', maximizeElement, true],
+        ['keydown', cancelSelection, true],
+    ];
 
-    // saved for restoration after hovering
-    var hoveredElement = null;
-    var savedBackground = null;
-    var savedOutline = null;
+    // Add all the listeners to the page
+    listeners.forEach(l => document.addEventListener(l[0], l[1], { once: l[2] }));
 
-    // saved for restoration after exiting fullscreen
-    var savedOverflow = null;
-    var savedHeight = null;
+    // Saved for restoration after hovering
+    let hoveredElement = null;
+    let savedBackground = null;
+    let savedOutline = null;
 
-    // listen for hovering over elements
-    // used for selecting the element to maximize
-    document.addEventListener('mouseover', event => {
-        if (maximizerSelectionActive !== true) return;
+
+
+    // Different steps in the maximization process
+
+    function highlightElement(event) {
         event.stopPropagation();
         event.preventDefault();
         if (event.target?.style instanceof CSSStyleDeclaration) {
@@ -55,52 +61,52 @@ function executeMaximizer() {
                 event.target.style.background = `rgba(${selectionColorRGB}, 0.25)`;
                 event.target.style.outline = `2px solid rgb(${selectionColorRGB})`;
             } catch (error) {
-                console.log('We catched an error', error);
+                console.debug('There was an error while highligting the element.', error);
             }
         }
         hoveredElement = event.target;
-    });
-
-    // listen for un-hovering elements
-    // used for resetting the style properties from the hovering over an element
-    document.addEventListener('mouseout', event => {
-        if (maximizerSelectionActive !== true) return;
+    }
+    
+    function unhighlightElement(event) {
         event.stopPropagation();
         event.preventDefault();
         resetElementStyle(hoveredElement)
-    });
-
-    // listen for clicking an element
-    // used for actually triggering the maximization of an element
-    document.addEventListener('click', event => {
-        if (maximizerSelectionActive === true) {
-            event.stopPropagation();
-            event.preventDefault();
-            event.target.requestFullscreen(event);
-            resetElementStyle(hoveredElement);
-            maximizerSelectionActive = false;
-        }
-    });
-
-    // make it possible to exit the maximization by pressing escape
-    document.addEventListener('keydown', event => {
+    }
+    
+    function maximizeElement(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        event.target.requestFullscreen(event);
+        resetElementStyle(hoveredElement);
+        removeListeners();
+    }
+    
+    function cancelSelection(event) {
         if (event.key === 'Escape') {
             event.stopPropagation();
             event.preventDefault();
             resetElementStyle(hoveredElement);
-            maximizerSelectionActive = false;
+            removeListeners();
         }
-    });
+    }
 
-    // reset an element's style after it has been hovered over for maximization selection
+
+
+    // Helper functions
+
     function resetElementStyle(element) {
         if (element?.style instanceof CSSStyleDeclaration) {
             try {
                 element.style.background = savedBackground;
                 element.style.outline = savedOutline;
             } catch (error) {
-                console.log("We catched an error!", error);
+                console.debug("There was an error resetting the elements style.", error);
             }
         }
+    }
+
+    // Removes the listeners on the tab and thereby ends the maximization process
+    function removeListeners() {
+        listeners.forEach(l => document.removeEventListener(l[0], l[1]));
     }
 }
